ec56dc24c7faa85b2b28dd7d7a8416bb892198c0
acyao <ac_yao94@hotmail.com>
2020-02-17 17:06:22 +0800

    Updates

---
+++ precache-manifest.2f824430b5962e5f877b50bff059f103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f930f6ce6d3c193d5e2e7888e9e15240",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "bded2ee3612b76300739",
+    "url": "/react-todo-app/static/css/main.f26bef13.chunk.css"
+  },
+  {
+    "revision": "f29de0b2a7c311a48b90",
+    "url": "/react-todo-app/static/js/2.fb2668cc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-todo-app/static/js/2.fb2668cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bded2ee3612b76300739",
+    "url": "/react-todo-app/static/js/main.a5f7059f.chunk.js"
+  },
+  {
+    "revision": "66c1052db373a7b29e49",
+    "url": "/react-todo-app/static/js/runtime-main.80c3587e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
